462770546bf244556a0413682d76b7fab52ff657
Yi Xiang <yx2365@columbia.edu>
2017-02-17 22:49:13 -0500

    update

--- app/ui.r
+++ app/ui.r
@@ -18,37 +18,40 @@ header <- dashboardHeader(
   titleWidth = 450
 )
 
-# USAcoord<-as.numeric(geocode("united states"))
-# ## Define UI for application that draws a histogram
-# points_starts<-data.frame(lon=-74,lat=40.7)
-# points_end<-data.frame(lon=-100,lat=30)
-# shinyUI(fluidPage(
-#   textInput("Origin", label = h3("Where do you want to start?"), value = "start point"),
-#   hr(),
-#   fluidRow(column(3, verbatimTextOutput("value"))),
-#     leafletOutput("mymap")
-#   )
-# )
+
 
 body<-dashboardBody(
-    fluidRow(
-      column(width = 9,
-             box(width = NULL, solidHeader = TRUE,
-                 leafletOutput("mymap", height = 500)
-             )
-             )
-      )
-    )
+  
+  tabItems(
+    #First Tab Item
+    tabItem(tabName = "routefinder",
+            fluidRow(
+              box(width = NULL, solidHeader = TRUE,
+                  leafletOutput("mymap", height = 500)
+              )
+            )
+    ),
+    
+    
+    tabItem(tabName = "statanalysis",
+            h2('This is the tab for statistical analysis'))
+  ))
+
+
+
+
+
 
 
 sidebar<-dashboardSidebar(
   sidebarMenu(
-    menuItem("RouteFinder", tabName = "Find a Route", icon = icon("map-marker")),
-    menuItem("StatAnalysis", tabName = "Data", icon = icon("signal"))
+    menuItem("RouteFinder", tabName = "routefinder", icon = icon("map-marker")),
+    menuItem("StatAnalysis", tabName = "statanalysis", icon = icon("signal"))
   )
 )
+
+
 
 dashboardPage(header,
               sidebar,
               body)
-
