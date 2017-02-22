7c2572ce848a14b53a12ee85b46367bfdd5702d5
YaqinLeahLi <yl3578@columbia.edu>
2017-02-22 15:55:09 -0500

    googleroute

---
+++ lib/googleroute.R
@@ -0,0 +1,66 @@
+##find nearby route of fueling stations
+library(RCurl)
+library(RJSONIO)
+library(leaflet)
+
+fuelstationicon<-icons("http://www.clker.com/cliparts/d/8/3/4/131550836610655076Gas%20Station%20Sign.svg.med.png",iconWidth = 18,iconHeight = 18)
+
+geocode<-function(add){
+  root<-"https://maps.google.com/maps/api/geocode/"
+  url<-paste0(root,"json?address=",add,"&key=AIzaSyC82ht4goSYy9M7Dp9tXc-vO9qxCoeF0jM")
+  u<-getURL(URLencode(url))
+  u<-fromJSON(u,simplify=FALSE)
+  if (u$status=="OK"){
+    lat<-as.numeric(u$results[[1]]$geometry$location$lat)
+    lng<-as.numeric(u$results[[1]]$geometry$location$lng)
+    location_type<-u$results[[1]]$geometry$location_type
+    formatted_address<-u$results[[1]]$formatted_address
+    return(c(lat,lng,location_type,formatted_address))
+  } else {
+    return(rep(NA,4))
+  }
+}
+
+
+
+get_nearbystations<-function(start,end){
+  #get the geocode of origin and destination
+  start <- "Columbia University in the city of new york"
+  end <- "Chicago University"
+  
+  startcoord <- geocode(start)[2:1]
+  endcoord <- geocode(end)[2:1]
+  
+  #get locations of fueling stations along the road  
+  fileUrl2<-paste("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearby-route.csv?api_key=Qf1NPRTeyq65qExWvjIVoGmqxyNu6QxYEHgFrZM4&route=LINESTRING(",startcoord[1],"+"
+                  ,startcoord[2],",",endcoord[1],"+",endcoord[2],")",sep = "")
+  download.file(fileUrl2,'./route.csv','curl')
+  nearbyfuelstations<-read.csv('./route.csv')
+  
+  return(nearbyfuelstations)
+}
+
+
+google_route<-function(start,end){
+  
+  #use google direction API to get the direction of two locations
+  myroute<-google_directions(origin = start,
+                             destination = end,
+                             mode = "driving",
+                             key = key)
+  
+  df_polyline <- decode_pl(myroute$routes$overview_polyline$points)
+  return(df_polyline)
+}
+
+mystations<-get_nearbystations(start,end)
+mygooglepoints<-google_route(start,end)
+
+#plot the fueling stations on the map
+leaflet() %>%
+  addTiles() %>%
+  addMarkers(data = mystations, lat = ~Latitude, lng = ~Longitude,icon =fuelstationicon) %>%
+  addPolylines(data = mygooglepoints,lat = ~lat, lng = ~lon)
+
+
+
