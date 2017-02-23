8907eb2f876720e8610bcc3eefec9bfd71abe918
Yue Gao <yg2474@columbia.edu>
2017-02-23 15:24:25 -0500

    fixed color/font in graphs
    use ggplot for everything, added notation

--- doc/Yue Gao/server.R
+++ doc/Yue Gao/server.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(plyr)
+library(ggplot2)
+
 
 shinyServer(
   function(input, output,session) {
@@ -8,51 +10,116 @@ shinyServer(
     output$model1=renderUI({
       c1=input$make1
       m1=as.vector(unique(vehdata$model[vehdata$manufacturer==c1]))
-      selectInput('model1', 'Select model:', m1)
+      rm1=as.vector(unique(vehdata$model[(vehdata$manufacturer==c1)
+                  &(vehdata$fueltype %in% c("Regular","Premium"))]))
+      am1=setdiff(m1,rm1)
+      selectInput('model1', 'Select model:', 
+                  choices=list("Alternative Fuel"=am1,
+                               "Regular Fuel"=rm1
+                               ))
       
     })
     
     output$model2=renderUI({
       c2=input$make2
       m2=as.vector(unique(vehdata$model[vehdata$manufacturer==c2]))
-      selectInput('model2', 'Select model:', m2)
+      rm2=as.vector(unique(vehdata$model[(vehdata$manufacturer==c2)
+                  &(vehdata$fueltype %in% c("Regular","Premium"))]))
+      am2=setdiff(m2,rm2)
+      selectInput('model2', 'Select model:', 
+                  choices=list(
+                    "Regular Fuel"=rm2,
+                    "Alternative Fuel"=am2
+                               
+                  ))
     })
     
     
     output$savespend=renderPlot({
       temp=fuel.avg[fuel.avg$fueltype %in% input$type,]
-      temp2=temp$savespend
+      temp=data.frame(temp)
       if(length(input$type>0)){
-      barplot(temp2,
-              main="Save/Spend on fuel costs in five years",
-              horiz=TRUE,names.arg=temp$fueltype,las=1,beside=TRUE)
-    }
+        c<-ggplot(data=temp,aes(x=fueltype,y=savespend,fill=fueltype))
+
+        (c+coord_flip()+geom_bar(stat="identity")
+          +theme(legend.position="none")
+          +ggtitle("Save/Spend on Fuel Costs in 5 Years")+
+                xlab("")+ ylab("Save/Spend in 5 years")
+          +theme(plot.title = element_text(size=22,face="bold",vjust=1,hjust=-1),
+                 axis.title.x = element_text(size=14,vjust=1,hjust=0.5),
+                 axis.text.y =element_text(size=14,face="bold"),
+                 plot.margin = unit(c(1,0.5,0.5,0.5),"cm")
+                 )
+          )
+
+          }
+      
     })
      
   output$mpg=renderPlot({
     temp=subset(fuel.avg,select=c(fueltype,combmpg,citympg,highwaympg))
     mpg=temp[temp$fueltype %in% input$type,]
-    mpg.m=subset(mpg,select=-fueltype)
-    mpg.m=t(mpg.m)
+    mpg=data.frame(fueltype1=rep(mpg$fueltype,3),
+        mpgtype=rep(c("Combined MPG","City MPG","Highway MPG"),each=nrow(mpg)),
+        dmpg=c(mpg$combmpg,mpg$citympg,mpg$highwaympg))
+    
+
     if(length(input$type>0)){
-    barplot(mpg.m,main="MPG for different fuels",xlab="fuels",
-            col=c("darkblue","red","yellow"),beside=TRUE,names.arg=mpg$fueltype,
-            legend=c("combined MPG","city MPG","highway MPG"))
+      
+      c<-ggplot(data=mpg,aes(x=fueltype1,y=dmpg,fill=mpgtype))
+      (c+geom_bar(stat="identity")+coord_flip()
+        +labs(title="MPG for Different Fuels",x="",
+         y ="MPG")+scale_fill_hue("Types of MPG")
+        +theme(plot.title = element_text(size=22,face="bold",vjust=1),
+               axis.title.x = element_text(size=14,vjust=1,hjust=0.5),
+               axis.text.y =element_text(size=14,face="bold"),
+               plot.margin = unit(c(1,0.5,0.5,0.5),"cm")
+        )
+      )
+      
     }
 })
 
   output$co2=renderPlot({
       temp=subset(fuel.avg,select = c(fueltype,co2))
       temp=temp[temp$fueltype %in% input$type,]
+      temp=data.frame(temp)
+      
       if(length(input$type>0)){
-      barplot(temp$co2,names.arg=temp$fueltype)}
+      
+        c<-ggplot(data=temp,aes(x=fueltype,y=co2,fill=fueltype))
+        (c+geom_bar(stat="identity")+coord_flip()
+          +theme(legend.position="none")
+          +labs(title="CO2 for Different Fuels",
+                x= "", y ="CO2")
+          +theme(plot.title = element_text(size=22,face="bold",vjust=1),
+                 axis.title.x = element_text(size=14,vjust=1,hjust=0.5),
+                 axis.text.y =element_text(size=14,face="bold"),
+                 plot.margin = unit(c(1,0.5,0.5,0.5),"cm")
+          )
+        )
+        
+        }
   })
   
   output$fuelcost=renderPlot({
     temp=subset(fuel.avg,select = c(fueltype,fuelcost))
     temp=temp[temp$fueltype %in% input$type,]
+    temp=data.frame(temp)
+    
     if(length(input$type>0)){
-    barplot(temp$fuelcost,names.arg=temp$fueltype)}
+      c<-ggplot(data=temp,aes(x=fueltype,y=fuelcost,fill=fueltype))
+      (c+geom_bar(stat="identity")+coord_flip()
+        +theme(legend.position="none")
+        +labs(title="Annual Fuel Cost for Different Fuels",
+              x= "", y ="Fuel Cost")
+        +theme(plot.title = element_text(size=22,face="bold",vjust=1),
+               axis.title.x = element_text(size=14,vjust=1,hjust=0.5),
+               axis.text.y =element_text(size=14,face="bold"),
+               plot.margin = unit(c(1,0.5,0.5,0.5),"cm")
+        )
+      )
+      }
   })
   
   output$table=renderTable({
@@ -83,7 +150,7 @@ shinyServer(
       }
 
 
-  print(m)
+  m
     
   })
   
--- doc/Yue Gao/ui.R
+++ doc/Yue Gao/ui.R
@@ -37,14 +37,39 @@ fluidPage(
   column(9,
   wellPanel(
     tabsetPanel(type = "tabs", 
-                tabPanel("Save/Spend", plotOutput("savespend")), 
-                tabPanel("MPG", plotOutput("mpg")), 
-                tabPanel("CO2", plotOutput("co2")),
-                tabPanel("Fuel Cost", plotOutput("fuelcost"))
+                tabPanel("Save/Spend", plotOutput("savespend"),
+                br(),
+                helpText("The Save/Spend here refers to by how much you save/spend over 5 years 
+                compared to an average new car. If your 5-year fuel cost is less than average,
+                which means you save money, the amount is positive; otherwise, you spend more than average,
+                the amount is negative. The calculation is based on 45% highway, 55% city driving, 15,000 annual miles and current fuel prices.")), 
+                
+                
+                tabPanel("MPG", plotOutput("mpg"),
+                br(),
+                helpText("MPG means Mile per Gallon. For electric and CNG vehicles this number
+                         is MPGe (gasoline equivalent miles per gallon). MPG deviates on the traffic condition,
+                         thus we show a result of city MPG, highway MPG and combined MPG (45% highway, 55% city driving).")
+                         ), 
+                
+                
+                tabPanel("CO2", plotOutput("co2"),
+
+                helpText("CO2 means tailpipe CO2 in grams/mile.")        
+                         ),
+                
+                
+                tabPanel("Fuel Cost", plotOutput("fuelcost"),
+
+                helpText("Annual fuel cost is based on 15,000 miles, 55% city driving, 
+                and the price of fuel used by the vehicle.
+                Fuel prices are from the Energy Information Administration."))
     )
   ),
   
-  wellPanel(tableOutput("table"))
+  wellPanel(tableOutput("table"),
+            helpText("For single fuel vehicles, there will be only one fuel. 
+                     For dual fuel vehicles, this will be a conventional fuel (fueltype1) and an alternative fuel (fueltype2)."))
   )
 
 )
