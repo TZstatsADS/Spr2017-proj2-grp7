2e447ade85c816a0bffe73c009c4312a1931e685
YaqinLeahLi <yl3578@columbia.edu>
2017-02-24 11:45:13 -0500

    update

--- lib/initial.R
+++
@@ -1,6 +0,0 @@
-fileUrl<-"https://developer.nrel.gov/api/alt-fuel-stations/v1.csv?api_key=Qf1NPRTeyq65qExWvjIVoGmqxyNu6QxYEHgFrZM4&state=NY"
-download.file(fileUrl,'./data.csv','curl')
-metadata<-read.csv('./data.csv')
-
-fileUrl2<-"https://developer.nrel.gov/api/alt-fuel-stations/v1/nearby-route.csv?api_key=Qf1NPRTeyq65qExWvjIVoGmqxyNu6QxYEHgFrZM4&route=LINESTRING(-74.0+40.7,-87.63+41.87,-104.98+39.76)"
-download.file(fileUrl2,'./route.csv','curl')
